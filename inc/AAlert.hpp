0ed2414c7db0b306b6959094010e750737db4cc6
Nicolas Buffon <akara@pixelized.fr>
2013-12-11 21:25:35 +0100

    Adding some attributes to AAlert class

--- inc/AAlert.hpp
+++ inc/AAlert.hpp
@@ -1,6 +1,7 @@
 #ifndef __AALERT_HPP__
 #define __AALERT_HPP__
 
+#include <QDateTime>
 #include <QString>
 
 /**
@@ -28,9 +29,80 @@ public:
 	 */
 	virtual void raise() = 0;
 
+	/**
+	 * \brief Sets the raising date of this alert
+	 *
+	 * \param date	Date
+	 */
+	inline void setDate(QDateTime date);
+
+	/**
+	 * \brief Returns the raising date of this alert
+	 *
+	 * \return the raising date
+	 */
+	inline QDateTime getDate();
+
+	/**
+	 * \brief Sets whether the alert is repetitive or not
+	 *
+	 * \param repetitive repetitive state for this alert
+	 */
+	inline void setRepetitive(bool repetitive);
+
+	/**
+	 * \brief Returns the repetitive state of this alert
+	 *
+	 * \return true if the alert is repetitive, false otherwise
+	 */
+	inline bool isRepetitive();
+	
+	/**
+	 * \brief Sets whether this alert is active or not
+	 *
+	 * \param active active state for this alert
+	 */
+	inline void setActive(bool active);
+
+	/**
+	 * \brief Returns the active state of this alert
+	 *
+	 * \return true if the alert is active, false otherwise 
+	 */
+	inline bool isActive();
+
 protected:
-	QString		_name;			///< \brief Name of the alert
-	QString		_description;	///< \brief Description of the alert
+	QString			_name;			///< \brief Name of the alert
+	QString			_description;	///< \brief Description of the alert
+
+	QDateTime		_date;			///< \brief Raising date
+	
+	bool 			_active;		///< \brief Is the alert active
+	bool			_repetitive;	///< \brief Is the alert repetitive
 };
+
+inline void AAlert::setDate(QDateTime date) {
+	_date = date;
+}
+
+inline QDateTime AAlert::getDate() {
+	return _date;
+}
+
+inline void AAlert::setRepetitive(bool repetitive) {
+	_repetitive = repetitive;
+}
+
+inline bool AAlert::isRepetitive() {
+	return _repetitive;
+}
+
+inline void AAlert::setActive(bool active) {
+	_active = active;
+}
+
+inline bool AAlert::isActive() {
+	return _active;
+}
 
 #endif // __AALERT_HPP__
