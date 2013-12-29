58d3c5081bd78abaa92a7b4cc1181931c2317ac5
Nicolas Buffon <akara@pixelized.fr>
2013-12-29 21:28:30 +0100

    Sample use of TickTimer added in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,17 @@
 #include <QApplication>
 
+#include "inc/AlertManager.hpp"
+#include "inc/TickTimer.hpp"
+
 int main(int argc, char** argv) {
     QApplication application(argc, argv);
+
+	AlertManager *manager = new AlertManager();
+
+	TickTimer *myAlert = new TickTimer("My alert", "Description of my alert", 45);
+
+	manager->addAlert(myAlert);
+	manager->start();
     
     return application.exec();
 }
