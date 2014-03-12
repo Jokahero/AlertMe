2154387b75a60b60d9fa6afb68c79fbdde6a76d7
Nicolas Buffon <jokahero@gmail.com>
2014-03-12 10:52:35 +0100

    Sound class documentation

--- inc/core/features/Sound.hpp
+++ inc/core/features/Sound.hpp
@@ -7,10 +7,26 @@ class QAudioOutput;
 class QSound;
 
 namespace Feature {
+	/*
+	 * \class Sound
+	 * \brief Sound feature
+	 * 
+	 * A feature tha allows alarm to play a sound on tick
+	 */
 	class Sound : public AFeature {
 
 	public:
+		/*
+		 * \brief Constructor
+		 *
+		 * \param label	Sound label
+		 * \param path	Sound path
+		 */
 		Sound(const QString &label, const QString &path);
+		
+		/*
+		 * \brief Destructor
+		 */
 		~Sound();
 
 		bool initialize();
@@ -20,10 +36,10 @@ namespace Feature {
 		virtual void play();
 
 	private:
-		QString			_label;
-		QSound			*_sound;
+		QString			_label;		///< \brief Sound's label
+		QSound			*_sound;	///< \brief Sound to be played
 
-		static const QString NAME;
+		static const QString NAME;		///< \brief Feature's name
 	};
 }
 #endif // __SOUND_HPP__
