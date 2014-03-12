52387b5eee1477a001ef3dcdca683d2fe2ffc0a8
Nicolas Buffon <jokahero@gmail.com>
2014-03-12 11:00:21 +0100

    AlertManager documentation

--- inc/core/AlertManager.hpp
+++ inc/core/AlertManager.hpp
@@ -5,17 +5,40 @@
 
 #include <QVector>
 
+/*
+ * \class AlertManager
+ * \brief Manages alerts
+ * 
+ * Manages a list of alerts
+ */
 class AlertManager {
 public:
+	/*
+	 * \brief Constructor
+	 */
 	AlertManager();
+	
+	/*
+	 * \brief Destructor
+	 */
 	virtual ~AlertManager();
 
+	/*
+	 * \brief Adds an alert in the list
+	 *
+	 * \param alert Alert to add
+	 */
 	inline void addAlert(AAlert* alert);
 
+	/*
+	 * \brief Starts the alerts
+	 *
+	 * Starts all active alerts
+	 */
 	void start();
 
 private:
-	QVector<AAlert*>	_alerts;
+	QVector<AAlert*>	_alerts;		///< \brief List of alerts
 
 	void startAlertTimer(AAlert* alert);
 };
