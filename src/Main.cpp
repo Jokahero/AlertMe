eb2ac432fd97c0b6026e05f06e66742e1848d503
Nicolas Buffon <jokahero@gmail.com>
2014-03-18 21:23:36 +0100

    Adding notification feature usage in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,24 +6,25 @@
 
 #include "core/AlertManager.hpp"
 #include "core/features/Sound.hpp"
+#include "core/features/Notification.hpp"
 #include "core/TickTimer.hpp"
 
 int main(int argc, char** argv) {
     QApplication application(argc, argv);
 
+	QSystemTrayIcon *icon = new QSystemTrayIcon(QIcon(":///icons/images/logo.png"));
+	icon->show();
+
 	AlertManager *manager = new AlertManager();
 
-	TickTimer *myAlert = new TickTimer("My alert", "Description of my alert", 900);
+	TickTimer *myAlert = new TickTimer("My alert", "Description of my alert", 60);
 	myAlert->addFeature(new Feature::Sound("Test sound", "://sounds/poke.wav"));
+	Feature::Notification* notification = new Feature::Notification("Hey, wake up !");
+	notification->setSystemTrayIcon(icon);
+	myAlert->addFeature(notification);
 
 	manager->addAlert(myAlert);
 	manager->start();
-
-    std::cout << (QSystemTrayIcon::isSystemTrayAvailable() ? "true" : "false") << std::endl;
-    
-	QSystemTrayIcon *icon = new QSystemTrayIcon(QIcon(":///icons/images/logo.png"));
-    icon->show();
-    icon->showMessage("Hey !", "Wake up !");
     
     return application.exec();
 }
