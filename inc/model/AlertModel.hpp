08c4dbe8d4fc8bc4f9b210bf4d1e8919d0f9e7f3
Nicolas Buffon <jokahero@gmail.com>
2015-08-30 19:41:00 +0200

    Model editing methods

--- inc/model/AlertModel.hpp
+++ inc/model/AlertModel.hpp
@@ -8,18 +8,29 @@ class AAlert;
 
 class AlertModel : public QAbstractListModel {
 public:
-    AlertModel(QVector<AAlert*> alerts, QObject *parent = 0);
+	static const int ActiveRole = Qt::UserRole;
+	static const int DelayRole = Qt::UserRole + 1;
+	static const int DesciptionRole = Qt::UserRole + 2;
+	static const int NameRole = Qt::UserRole + 3;
+	static const int RepetitiveRole = Qt::UserRole + 4;
 
-    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
-    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
-    //virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+	AlertModel(QVector<AAlert*> alerts, QObject *parent = 0);
+
+	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+	virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+	virtual QMap<int, QVariant> itemData(const QModelIndex &index) const;
+
+	virtual bool setData(const QModelIndex &index, const QVariant &value, int role);
+	virtual bool setItemData(const QModelIndex &index, const QMap<int, QVariant> &roles);
 
 signals:
 
 public slots:
 
 private:
-    QVector<AAlert*> _alerts;
+	QVector<AAlert*> _alerts;
+
+	bool isIndexValid(const QModelIndex &index) const;
 };
 
 #endif // ALERTMODEL_H
--- src/model/AlertModel.cpp
+++ src/model/AlertModel.cpp
@@ -7,23 +7,94 @@
 AlertModel::AlertModel(QVector<AAlert*> alerts, QObject *parent) : QAbstractListModel(parent), _alerts(alerts) {}
 
 int AlertModel::rowCount(const QModelIndex &parent) const {
-    UNUSED(parent);
+	UNUSED(parent);
 
-    return _alerts.size();
+	return _alerts.size();
 }
 
 QVariant AlertModel::data(const QModelIndex &index, int role) const {
-    if (!index.isValid())
-        return QVariant();
+	if (!isIndexValid(index))
+		return QVariant();
 
-    int row = index.row();
-    if (row < 0 || row >= _alerts.size())
-        return QVariant();
+	int row = index.row();
 
-    switch (role) {
-        case Qt::DisplayRole:
-            return _alerts.at(row)->name();
-    }
+	switch (role) {
+		case AlertModel::ActiveRole:
+			return _alerts.at(row)->isActive();
+		case Qt::DisplayRole:
+		case AlertModel::NameRole:
+			return _alerts.at(row)->name();
+		case AlertModel::DelayRole:
+			return QVariant();
+		case AlertModel::DesciptionRole:
+			return _alerts.at(row)->description();
+		case AlertModel::RepetitiveRole:
+			return _alerts.at(row)->isRepetitive();
+	}
 
-    return QVariant();
+	return QVariant();
+}
+
+QMap<int, QVariant> AlertModel::itemData(const QModelIndex &index) const {
+	if (!isIndexValid(index))
+		return QMap<int, QVariant>();
+
+	QMap<int, QVariant> map = QAbstractListModel::itemData(index);
+
+	AAlert *alert = _alerts.at(index.row());
+
+	map.insert(AlertModel::ActiveRole, alert->isActive());
+	map.insert(AlertModel::DelayRole, QVariant()); // FIXME
+	map.insert(AlertModel::DesciptionRole, alert->description());
+	map.insert(AlertModel::NameRole, alert->name());
+	map.insert(AlertModel::RepetitiveRole, alert->isRepetitive());
+
+	return map;
+}
+
+bool AlertModel::isIndexValid(const QModelIndex &index) const {
+	if (index.isValid()) {
+		int row = index.row();
+		if (row >= 0 && row < _alerts.size())
+			return true;
+	}
+	return false;
+}
+
+bool AlertModel::setData(const QModelIndex &index, const QVariant &value, int role) {
+	if (!isIndexValid(index))
+		return false;
+
+	AAlert *alert = _alerts.at(index.row());
+
+	switch (role) {
+		case AlertModel::ActiveRole:
+			alert->setActive(value.toBool());
+			return true;
+		case AlertModel::DesciptionRole:
+			alert->setDescription(value.toString());
+			return true;
+		case AlertModel::NameRole:
+			alert->setName(value.toString());
+			return false;
+		case AlertModel::RepetitiveRole:
+			alert->setRepetitive(value.toBool());
+			return true;
+	}
+
+	return false;
+}
+
+bool AlertModel::setItemData(const QModelIndex &index, const QMap<int, QVariant> &roles) {
+	if (!isIndexValid(index))
+		return false;
+
+	AAlert *alert = _alerts.at(index.row());
+
+	alert->setActive(roles.value(AlertModel::ActiveRole).toBool());
+	alert->setDescription(roles.value(AlertModel::DesciptionRole).toString());
+	alert->setName(roles.value(AlertModel::NameRole).toString());
+	alert->setRepetitive(roles.value(AlertModel::RepetitiveRole).toBool());
+
+	return true;
 }
