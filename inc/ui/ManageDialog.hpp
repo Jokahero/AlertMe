7910fba1b71989cc59bec2ebd829fe487f76d359
Nicolas Buffon <jokahero@gmail.com>
2015-08-30 20:21:01 +0200

    Interaction between model and view

--- inc/ui/ManageDialog.hpp
+++ inc/ui/ManageDialog.hpp
@@ -4,23 +4,40 @@
 #include <QDialog>
 
 namespace Ui {
-    class ManageDialog;
+	class ManageDialog;
 }
 
 class AlertModel;
 
 class ManageDialog : public QDialog {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit ManageDialog(AlertModel *model, QWidget *parent = 0);
-    ~ManageDialog();
+	explicit ManageDialog(AlertModel *model, QWidget *parent = 0);
+	~ManageDialog();
 
 private slots:
-    void updateAlertDetails(const QModelIndex &index);
+	/**
+	 * @brief Fill the fields with the details of a specific alert
+	 *
+	 * @param index QModelIndex of the alert in the model
+	 */
+	void updateAlertDetails(const QModelIndex &index);
+
+	/**
+	 * @brief Sets the content of the fields back to their original value
+	 */
+	void reset();
+
+	/**
+	 * @brief Update the model data with the values filled in the fields
+	 */
+	void updateModel();
 
 private:
-    Ui::ManageDialog *_ui;
+	Ui::ManageDialog *_ui;
+
+	AlertModel *_model;
 };
 
 #endif // MANAGEDIALOG_HPP
--- src/ui/ManageDialog.cpp
+++ src/ui/ManageDialog.cpp
@@ -5,20 +5,40 @@
 
 #include <QDebug>
 
-ManageDialog::ManageDialog(AlertModel*model, QWidget *parent) : QDialog(parent), _ui(new Ui::ManageDialog) {
-    _ui->setupUi(this);
+ManageDialog::ManageDialog(AlertModel*model, QWidget *parent) : QDialog(parent), _ui(new Ui::ManageDialog), _model(model) {
+	_ui->setupUi(this);
 
-    connect(_ui->closeButton, &QPushButton::clicked, this, &QDialog::accept);
-    connect(_ui->alertsListView, &QListView::clicked, this, &ManageDialog::updateAlertDetails);
+	_ui->alertsListView->setModel(model);
+	_ui->alertsListView->setCurrentIndex(model->index(0));
+	updateAlertDetails(_ui->alertsListView->currentIndex());
 
-    _ui->alertsListView->setModel(model);
-    _ui->alertsListView->setCurrentIndex(model->index(0));
+	connect(_ui->closeButton, &QPushButton::clicked, this, &QDialog::accept);
+	connect(_ui->alertsListView, &QListView::clicked, this, &ManageDialog::updateAlertDetails);
+	connect(_ui->resetButton, &QPushButton::clicked, this, &ManageDialog::reset);
+	connect(_ui->updateButton, &QPushButton::clicked, this, &ManageDialog::updateModel);
 }
 
 ManageDialog::~ManageDialog() {
-    delete _ui;
+	delete _ui;
 }
 
 void ManageDialog::updateAlertDetails(const QModelIndex &index) {
-    _ui->labelEdit->setText(index.data().toString());
+	QMap<int, QVariant> dataMap = _ui->alertsListView->model()->itemData(index);
+	_ui->labelEdit->setText(dataMap.value(AlertModel::NameRole).toString());
+	_ui->descriptionEdit->setText(dataMap.value(AlertModel::DesciptionRole).toString());
+	_ui->repetitiveCheckbox->setChecked(dataMap.value(AlertModel::RepetitiveRole).toBool());
+}
+
+void ManageDialog::reset() {
+	updateAlertDetails(_ui->alertsListView->currentIndex());
+}
+
+void ManageDialog::updateModel() {
+	QMap<int, QVariant> dataMap;
+
+	dataMap.insert(AlertModel::RepetitiveRole, _ui->repetitiveCheckbox->isChecked());
+	dataMap.insert(AlertModel::NameRole, _ui->labelEdit->text());
+	dataMap.insert(AlertModel::DesciptionRole, _ui->descriptionEdit->text());
+
+	_model->setItemData(_ui->alertsListView->currentIndex(), dataMap);
 }
