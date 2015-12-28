bc2ff4a499e43c09e57f45c203f0771fa76380cb
Nicolas Buffon <jokahero@gmail.com>
2015-12-28 01:50:53 +0100

    New app logo

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,32 +15,32 @@
 #include "ui/ManageDialog.hpp"
 
 int main(int argc, char** argv) {
-    QApplication application(argc, argv);
-    application.setQuitOnLastWindowClosed(false);
+	QApplication application(argc, argv);
+	//application.setQuitOnLastWindowClosed(false);
 
-    // System tray icon -----------------------------------------------------------------
+	// System tray icon -----------------------------------------------------------------
 
-	QSystemTrayIcon *icon = new QSystemTrayIcon(QIcon(":///icons/images/logo.png"));
+	QSystemTrayIcon *icon = new QSystemTrayIcon(QIcon(":///icons/images/bell.png"));
 	icon->show();
 
-    QMenu* menu = new QMenu();
-    QAction* manageAction = menu->addAction(QIcon(":///icons/images/manage.png"), QAction::tr("Ma&nage"));
-    menu->addSeparator();
-    QAction* quitAction = menu->addAction(QIcon(":///icons/images/quit.png"), QAction::tr("&Quitter"));
+	QMenu* menu = new QMenu();
+	QAction* manageAction = menu->addAction(QIcon(":///icons/images/manage.png"), QAction::tr("Ma&nage"));
+	menu->addSeparator();
+	QAction* quitAction = menu->addAction(QIcon(":///icons/images/quit.png"), QAction::tr("&Quitter"));
 
-    icon->setContextMenu(menu);
+	icon->setContextMenu(menu);
 
-    QObject::connect(quitAction, &QAction::triggered, qApp, &QApplication::quit);
+	QObject::connect(quitAction, &QAction::triggered, qApp, &QApplication::quit);
 
-    // Alerts ---------------------------------------------------------------------------
-    AlertManager *manager = new AlertManager();
+	// Alerts ---------------------------------------------------------------------------
+	AlertManager *manager = new AlertManager();
 
 	/** Look around **/
 	Feature::Notification* eyesNotification = new Feature::Notification("Hey, look around !");
 	eyesNotification->setSystemTrayIcon(icon);
 
 	TickTimer *eyesAlert = new TickTimer("Eyes alert", "You should look away", 900);
-	eyesAlert->addFeature(new Feature::Sound("Test sound", "://sounds/poke.wav"));
+	//eyesAlert->addFeature(new Feature::Sound("Test sound", "://sounds/poke.wav"));
 	eyesAlert->addFeature(eyesNotification);
 
 	/** Walk around **/
